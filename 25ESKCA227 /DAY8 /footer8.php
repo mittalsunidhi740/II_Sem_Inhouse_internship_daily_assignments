bee3ed05d11511278047939b2b72caf183e11ca6
mittalsunidhi740 <mittalsunidhi740@gmail.com>
2026-07-12 16:33:03 +0530

    Create footer8.php

---
+++ 25ESKCA227 /DAY8 /footer8.php
@@ -0,0 +1,18 @@
+</div>
+
+    <footer class="bg-dark text-white text-center p-3 mt-5">
+        <p class="mb-0">
+            <i class="fa-solid fa-code"></i>
+            Student Registration Confirmation System
+        </p>
+
+        <small>
+            © <?php echo date("Y"); ?> All Rights Reserved
+        </small>
+    </footer>
+
+    <!-- Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+
+</body>
+</html>
